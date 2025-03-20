e99dc33150cca731d379415645b82da39863306c
OAKDev <oak99.dev@gmail.com>
2025-03-19 21:23:12 -0300

    chore: font increase

--- app/_components/footer.tsx
+++ app/_components/footer.tsx
@@ -7,14 +7,14 @@ const Footer = () => {
       
       <div className="flex flex-col container mx-auto px-4 py-16 sm:flex-row justify-between     p-4 sm:p-8 text-center sm:text-left">
         <div className=" text-base flex flex-col items-center sm:items-start mb-4 sm:mb-0">
-          <Image src="/logoW.png" width={120} height={70} alt="Logo Organic Reach Rewards White" />
-          <h1 className="mt-2 font-bold max-w-md">
+          <Image src="/logoW.png" width={150} height={90} alt="Logo Organic Reach Rewards White" />
+          <h1 className="mt-2 text-2xl font-bold max-w-md">
             “Grow organically, build customer loyalty and transform engagement
             into real results. Your business, driven naturally!”
           </h1>
         </div>
 
-        <div className="flex flex-col justify-center sm:justify-center items-center  gap-4">
+        <div className="flex flex-col justify-center sm:justify-center text-xl items-center  gap-4">
           <Link href="/">Careers</Link>
           <Link href="/">Terms of Service</Link>
           <Link href="/">Privacy Policy</Link>
