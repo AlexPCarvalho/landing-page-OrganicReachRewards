2a396c179d5497bbbb38559c4bad8ac2f6815ce6
OAKDev <oak99.dev@gmail.com>
2025-04-02 01:00:52 -0300

    fix: Disables the unescaped entities rule

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,11 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "react/no-unescaped-entities": "off", // Desabilita a regra de entidades não escapadas
+    },
+  },
 ];
 
 export default eslintConfig;
